644707c61f2f2dad4d3e3da5139a5a17954d4222
Vinicius Rodrigues da Silva <vinicius.rodrigues2004@gmail.com>
2024-10-17 22:09:32 -0300

    Criando a interface MercadoBitcoinService.kt e a classe MercadoBitcoinServiceFactory para a chamada

---
+++ app/src/main/java/vinighub/com/github/cryptomonitor/services/MercadoBitcoinService.kt
@@ -0,0 +1,9 @@
+package vinighub.com.github.cryptomonitor.services
+
+import vinighub.com.github.cryptomonitor.model.TickerResponse
+import retrofit2.Response
+import retrofit2.http.GET
+interface MercadoBitcoinService {
+    @GET("api/BTC/ticker/")
+    suspend fun getTicker(): Response<TickerResponse>
+}
